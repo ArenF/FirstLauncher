89a79a39ffa681edd98d0c778bb5020d77b4311f
Allen <hjh040@gmail.com>
2022-09-25 23:40:30 +0900

    cx_freeze 도입, 빌드 생성

--- setup.py
+++ setup.py
@@ -1,37 +1,27 @@
-from setuptools import setup
-import py2exe
+#setup.py
+
 import os
 
-def get_cefpython_path():
-    import cefpython3 as cefpython
-    path = os.path.dirname(cefpython.__file__)
-    return "%s%s" % (path, os.sep)
+from distutils.sysconfig import get_python_lib
+from os.path import join
+from glob import glob
+from cx_Freeze import setup, Executable
 
-def get_data_files():
-    cefp = get_cefpython_path()
-    data_files = [('', [
-          '%s/icudt.dll' % cefp,
-          '%s/d3dcompiler_43.dll' % cefp,
-          '%s/d3dx9_43.dll' % cefp,
-          '%s/devtools_resources.pak' % cefp,
-          '%s/ffmpegsumo.dll' % cefp,
-          '%s/libEGL.dll' % cefp,
-          '%s/libGLESv2.dll' % cefp,
-          '%s/Microsoft.VC90.CRT.manifest' % cefp,
-          '%s/msvcm90.dll' % cefp,
-          '%s/msvcp90.dll' % cefp,
-          '%s/msvcr90.dll' % cefp,
-          'icon.ico', 'cefsimple.html', 'client', 'html', 'blob_storage', 'webrtc_event_logs']),
-        ('locales', ['%s/locales/en-US.pak' % cefp]),
-        ]
-    return data_files
+# Basically just copy all of the CEF distribution into the installer
+# I have only tested this on windows
+cefPath = join(get_python_lib(), "cefpython3")
+CEF_INCLUDES = glob(join(cefPath, "*"))
+CEF_INCLUDES.remove(join(cefPath, "examples"))
 
 setup(
-    data_files = get_data_files(),
-    windows=['cefsimple.py'],
-    options={
-        "py2exe": {
-            "includes": ["json", "urllib"]
-        }
-    }
-)
+    name = "First Launcher",
+    version = "1.0.0",
+    author = "ALLEN",
+    options = {
+        "build_exe": {
+        'packages': ["os","sys","ctypes","win32con", "json", "platform", "threading", "minecraft_launcher_lib"],
+        'include_files': CEF_INCLUDES + [('html')],
+        'include_msvcr': True,
+    }},
+    executables = [Executable('main.py')]
+) 
